50e5a5330f01aaa7e9502fc29133960693b31bb9
boxframetech <boxframe@protonmail.com>
2021-08-17 16:04:36 +0000

    Word-dictionary first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useEffect, useState} from 'react'
+import {Container} from '@material-ui/core'
+import Header from './Components/Header'
+
+let dictionaryApiUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/duck'
 
 function App() {
+
+  const [word,setWord] = useState('')
+
+  const [meanings,setMeanings] = useState([])
+
+  const dictionaryApi = async() => {
+    try{
+
+      const data = await axios.get(dictionaryApiUrl)
+
+      setMeanings(data.data)
+
+    }catch(error){
+
+      console.log(error)
+
+    }
+  }
+
+  console.log(meanings)
+
+  useEffect(() => {
+      dictionaryApi()
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Container maxWidth='md' className='d-flex'>
+        <Header/>
+     </Container>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        
+            <div className="header">
+                <span className="title">
+                        Word Dictionary
+                </span>
+            </div>
+            
+    )
+}
+
+export default Header
